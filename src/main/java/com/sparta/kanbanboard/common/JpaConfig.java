0edee785d326c411681efb57bf6c77640307e2f9
leebeanbin <wjdqlsdy1004@naver.com>
2024-07-10 23:26:32 +0900

    move + refactor : move jap congif file and renaming it

--- src/main/java/com/sparta/kanbanboard/common/JpaConfig.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.kanbanboard.common;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing
-public class JpaConfig {
-}
