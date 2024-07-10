ac8f237eac2f33340fd6c4426df37585fd4c8e72
leebeanbin <wjdqlsdy1004@naver.com>
2024-07-10 23:26:32 +0900

    move + refactor : move jap congif file and renaming it

--- src/main/java/com/sparta/kanbanboard/config/JpaAuditingConfig.java
+++ src/main/java/com/sparta/kanbanboard/config/JpaAuditingConfig.java
@@ -1,9 +1,9 @@
-package com.sparta.kanbanboard.common;
+package com.sparta.kanbanboard.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing
-public class JpaConfig {
+public class JpaAuditingConfig {
 }
