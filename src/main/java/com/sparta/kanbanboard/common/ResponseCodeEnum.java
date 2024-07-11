ec7f000026c72e851c180a5aa711d4fddd9e2861
bbackbbacke <157469179+bbackbbacke@users.noreply.github.com>
2024-07-11 17:22:21 +0900

    add : InvailCardDataException 추가

--- src/main/java/com/sparta/kanbanboard/common/ResponseCodeEnum.java
+++ src/main/java/com/sparta/kanbanboard/common/ResponseCodeEnum.java
@@ -7,13 +7,12 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ResponseCodeEnum {
-    // 댓글
-    CREATE_COMMENT_SUCCESS(HttpStatus.CREATED, "댓글이 작성되었습니다."),
-    GET_COMMENTS(HttpStatus.OK, null),
-    UPDATE_COMMENT_SUCCESS(HttpStatus.OK, "댓글 수정에 성공하였습니다."),
-    DELETE_COMMENT_SUCCESS(HttpStatus.OK, "댓글이 삭제되었습니다."),
     // 유저
-    USER_SUCCESS_SIGNUP(HttpStatus.OK, "님의 회원가입을 완료 했습니다.")
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다"),
+    USER_DELETED(HttpStatus.UNAUTHORIZED, "탈퇴한 사용자입니다"),
+
+    // 카드
+    INVALID_CARD_DATA(HttpStatus.NOT_FOUND, "카드 필수 데이터가 없습니다.")
     ;
     private final HttpStatus httpStatus;
     private final String message;
--- src/main/java/com/sparta/kanbanboard/exception/card/InvailCardDataException.java
+++ src/main/java/com/sparta/kanbanboard/exception/card/InvailCardDataException.java
@@ -2,8 +2,8 @@ package com.sparta.kanbanboard.exception.card;
 
 import com.sparta.kanbanboard.common.ResponseCodeEnum;
 
-public class CardNotFoundException extends CardException{
-    public CardNotFoundException(ResponseCodeEnum responseCodeEnum) {
+public class InvailCardDataException extends CardException{
+    public InvailCardDataException(ResponseCodeEnum responseCodeEnum) {
         super(responseCodeEnum);
     }
     // 제목, 카드상태, 내용 필수 데이터가 존재하지 않는 경우
