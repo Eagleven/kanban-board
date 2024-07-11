5d5613892589cb7db4d55bc79a4b91c3da6fa722
bbackbbacke <157469179+bbackbbacke@users.noreply.github.com>
2024-07-11 19:00:03 +0900

    fix : Card Entity 수정 (카드 상태 status를 column에서 받아 쓰기로 함 연관관계 mapping 이후에 수정필요)

--- src/main/java/com/sparta/kanbanboard/domain/card/entity/Card.java
+++ src/main/java/com/sparta/kanbanboard/domain/card/entity/Card.java
@@ -3,9 +3,12 @@ package com.sparta.kanbanboard.domain.card.entity;
 import com.sparta.kanbanboard.common.TimeStampEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -19,7 +22,7 @@ import lombok.Setter;
 @Table(name = "cards")
 public class Card extends TimeStampEntity {
 
-    @Column(name = "cardId")
+    @Column
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -30,11 +33,13 @@ public class Card extends TimeStampEntity {
     @Column(nullable = false)
     private String contents;
 
+
 //    @OneToMany(mappedBy = "card")
-//    private Long userId;
+//    private User user;
 //
-//    @OneToMany(mappedBy = "card")
-//    private Long columnId;
+//    @ManyToOne    =====> 여기서 column이랑 연관관계 맺고 나서 status enum 불러와서 수정 처리 하기
+//    @JoinColumn(name = "column_id")
+//    private Column column;
 
 
     public void update(String title, String contents) {
