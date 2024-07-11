cef24b25b0b9fb415af4664e1b8aa0b3ab714c82
bbackbbacke <157469179+bbackbbacke@users.noreply.github.com>
2024-07-11 19:01:33 +0900

    add : custom exception추가

--- src/main/java/com/sparta/kanbanboard/exception/user/UserNotFoundException.java
+++ src/main/java/com/sparta/kanbanboard/exception/user/UserNotFoundException.java
@@ -1,9 +1,11 @@
 package com.sparta.kanbanboard.exception.user;
 
 import com.sparta.kanbanboard.common.ResponseCodeEnum;
+import com.sparta.kanbanboard.common.ResponseExceptionEnum;
+import com.sparta.kanbanboard.exception.card.UserException;
 
 public class UserNotFoundException extends UserException {
-    public UserNotFoundException(ResponseCodeEnum responseCodeEnum) {
-        super(responseCodeEnum);
+    public UserNotFoundException(ResponseExceptionEnum responseExceptionEnum) {
+        super(responseExceptionEnum);
     }
 }
