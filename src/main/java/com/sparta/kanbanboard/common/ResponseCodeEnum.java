780cbecd9d7f24251bdd3b978a3e1b50b99daac4
bbackbbacke <157469179+bbackbbacke@users.noreply.github.com>
2024-07-11 17:22:21 +0900

    add : InvailCardDataException 추가

--- src/main/java/com/sparta/kanbanboard/common/ResponseCodeEnum.java
+++ src/main/java/com/sparta/kanbanboard/common/ResponseCodeEnum.java
@@ -9,7 +9,10 @@ import org.springframework.http.HttpStatus;
 public enum ResponseCodeEnum {
     // 유저
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다"),
-    USER_DELETED(HttpStatus.UNAUTHORIZED, "탈퇴한 사용자입니다")
+    USER_DELETED(HttpStatus.UNAUTHORIZED, "탈퇴한 사용자입니다"),
+
+    // 카드
+    INVALID_CARD_DATA(HttpStatus.NOT_FOUND, "카드 필수 데이터가 없습니다.")
     ;
     private final HttpStatus httpStatus;
     private final String message;
