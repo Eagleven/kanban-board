bc161475be68b1c1fe99971d96d52aa36d0ac05c
bbackbbacke <157469179+bbackbbacke@users.noreply.github.com>
2024-07-11 19:00:42 +0900

    add : repository, adapter 추가, findByStatus부분 column이랑 매핑 후에 수정 필요

--- src/main/java/com/sparta/kanbanboard/domain/card/repository/CardAdapter.java
+++ src/main/java/com/sparta/kanbanboard/domain/card/repository/CardAdapter.java
@@ -1,5 +1,8 @@
 package com.sparta.kanbanboard.domain.card.repository;
 
+import com.sparta.kanbanboard.domain.card.entity.Card;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -9,4 +12,35 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class CardAdapter {
 private final CardRepository cardRepository;
+
+// 예외처리도 이곳에서 해줘야함!
+public List<Card> findAll() {
+    return cardRepository.findAll();
+}
+
+public List<Card> findByUserId(Long userId) {
+    return cardRepository.findByUserId(userId);
+}
+
+//    public List<Card> findByStatus(CardStatusEnum status) {
+//    return cardRepository.findByStatus(status);
+//}
+
+public Optional<Card> findById(Long cardId) {
+    return cardRepository.findById(cardId);
+}
+
+public Card save(Card card) {
+    return cardRepository.save(card);
+}
+
+public void delete(Card card) {
+    cardRepository.delete(card);
+}
+
+
+
+
+
+
 }
--- src/main/java/com/sparta/kanbanboard/domain/card/repository/CardRepository.java
+++ src/main/java/com/sparta/kanbanboard/domain/card/repository/CardRepository.java
@@ -1,10 +1,14 @@
 package com.sparta.kanbanboard.domain.card.repository;
 
 import com.sparta.kanbanboard.domain.card.entity.Card;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CardRepository extends JpaRepository<Card,Long> {
+    List<Card> findByUserId(Long userId);
+//    List<Card> findByStatus(ColumnStatusEnum status);
+
 
 }
