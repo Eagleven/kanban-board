e5a613771f5f8d21a23d07c10c8dbaffed093bf8
bbackbbacke <157469179+bbackbbacke@users.noreply.github.com>
2024-07-11 23:03:57 +0900

    add : user과 연관관계 맺음(ManyToOne), user 생성자에 추가

--- src/main/java/com/sparta/kanbanboard/domain/card/entity/Card.java
+++ src/main/java/com/sparta/kanbanboard/domain/card/entity/Card.java
@@ -1,11 +1,14 @@
 package com.sparta.kanbanboard.domain.card.entity;
 
 import com.sparta.kanbanboard.common.TimeStampEntity;
+import com.sparta.kanbanboard.domain.user.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
@@ -30,18 +33,17 @@ public class Card extends TimeStampEntity {
     @Column(nullable = false)
     private String contents;
 
-//    @OneToMany(mappedBy = "card")
-//    private User user;
-//
-//    @ManyToOne    =====> 여기서 column이랑 연관관계 맺고 나서 status enum 불러와서 수정 처리 하기
-//    @JoinColumn(name = "column_id")
-//    private Column column;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
+    // Column : Card는 1:N 추가해야함
 
     @Builder
-    public Card(String title, String contents /*, Long columnId*/) {
+    public Card(String title, String contents, User user/*, Long columnId*/) {
         this.title = title;
         this.contents = contents;
+        this.user = user;
         // this.columnId = columnId;
     }
 
@@ -49,6 +51,4 @@ public class Card extends TimeStampEntity {
         this.title = title;
         this.contents = contents;
     }
-
-
 }
