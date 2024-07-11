3fe81e62b31a42a9214ec6985187f33dd433b00c
M1ngD0ng <alswjd6807@naver.com>
2024-07-11 22:06:52 +0900

    merge : from dev

--- src/main/java/com/sparta/kanbanboard/common/ResponseExceptionEnum.java
+++ src/main/java/com/sparta/kanbanboard/common/ResponseExceptionEnum.java
@@ -11,8 +11,9 @@ public enum ResponseExceptionEnum {
     INVALID_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 입력입니다."),
 
     // 유저
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다"),
-    USER_DELETED(HttpStatus.UNAUTHORIZED, "탈퇴한 사용자입니다"),
+    SUCCESS_LOGIN(HttpStatus.OK, "로그인을 완료했습니다."),
+    USER_SUCCESS_SIGNUP(HttpStatus.OK, "님의 회원가입을 완료 했습니다."),
+    REISSUE_ACCESS_TOKEN(HttpStatus.OK, "억세스 토큰 재발급을 완료했습니다."),
 
     // board
     BOARD_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 보드입니다."),
