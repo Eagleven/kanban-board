06cac27018a809809a72c038ef7f99a44b78edf8
bomin <seo78200@gmail.com>
2024-07-11 10:28:49 +0900

    feat : 댓글 생성 기능

--- src/main/java/com/sparta/kanbanboard/domain/comment/controller/CommentController.java
+++ src/main/java/com/sparta/kanbanboard/domain/comment/controller/CommentController.java
@@ -6,9 +6,7 @@ import com.sparta.kanbanboard.common.ResponseExceptionEnum;
 import com.sparta.kanbanboard.common.ResponseUtils;
 import com.sparta.kanbanboard.domain.comment.dto.CommentDto;
 import com.sparta.kanbanboard.domain.comment.dto.CommentResponseDto;
-import com.sparta.kanbanboard.domain.comment.entity.Comment;
 import com.sparta.kanbanboard.domain.comment.service.CommentService;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,39 +31,21 @@ public class CommentController {
         if (result == null) {
             return ResponseUtils.of(ResponseExceptionEnum.CREATE_COMMENT_FAILURE);
         }
-        return ResponseUtils.of(ResponseCodeEnum.CREATE_COMMENT_SUCCESS, result);
+        return ResponseUtils.of(ResponseCodeEnum.CREATE_COMMENT_SUCCESS, commentService.createComment(cardId, commentDto));
     }
 
-    @GetMapping("/{cardId}")
-    public ResponseEntity<HttpResponseDto> getCommentsList(@PathVariable("cardId") Long cardId) {
-        List<CommentResponseDto> result = commentService.getCommentsList(cardId);
-        if (result == null) {
-            return ResponseUtils.of(ResponseExceptionEnum.CARD_NOT_FOUND);
-        }
-        return ResponseUtils.of(ResponseCodeEnum.GET_COMMENTS, result);
-    }
-
-    @PutMapping("/{commentId}")
-    public ResponseEntity<HttpResponseDto> updateComment(@PathVariable("commentId") Long commentId, @RequestBody CommentDto commentDto) {
-        CommentResponseDto result = commentService.updateComment(commentId, commentDto);
-        if (result == null) {
-            if (commentDto.getContent() == null) {
-                return ResponseUtils.of(ResponseExceptionEnum.COMMENT_CONTENT_REQUIRED);
-            } else {
-                return ResponseUtils.of(ResponseExceptionEnum.COMMENT_NOT_FOUND);
-            }
-        }
-
-        return ResponseUtils.of(ResponseCodeEnum.UPDATE_COMMENT_SUCCESS, result);
-    }
-
-    @PatchMapping("/{commentId}")
-    public ResponseEntity<HttpResponseDto> deleteComment(@PathVariable("commentId") Long commentId) {
-        Comment result = commentService.deleteComment(commentId);
-        if (result == null) {
-            return ResponseUtils.of(ResponseExceptionEnum.COMMENT_NOT_FOUND);
-        }
-
-        return ResponseUtils.of(ResponseCodeEnum.DELETE_COMMENT_SUCCESS);
-    }
+//    @GetMapping("/{cardId}")
+//    public ResponseEntity<HttpResponseDto> getCommentsList(@PathVariable("cardId") Long cardId) {
+//        return ResponseUtils.of(ResponseCodeEnum.COMMENT_CREATED, commentService.getCommentsList(cardId));
+//    }
+//
+//    @PutMapping("/{commentId}")
+//    public ResponseEntity<HttpResponseDto> updateComment(@PathVariable("commentId") Long commentId, @RequestBody CommentDto requestDto) {
+//        return ResponseUtils.of(ResponseCodeEnum.COMMENT_CREATED, commentService.updateComment(commentId, requestDto));
+//    }
+//
+//    @PatchMapping("/{commentId}")
+//    public ResponseEntity<HttpResponseDto> deleteComment(@PathVariable("commentId") Long commentId) {
+//        return ResponseUtils.of(ResponseCodeEnum.COMMENT_CREATED, commentService.deleteComment(commentId));
+//    }
 }
