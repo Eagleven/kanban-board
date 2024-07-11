41a5137c10e28b6cd2fe7f8fbf02724e5947d979
bbackbbacke <157469179+bbackbbacke@users.noreply.github.com>
2024-07-11 20:28:03 +0900

    add :**질문있어요 확인요망!주석처리확인부탁드려요** CardService단에 column관련해서 추가사항있음

--- src/main/java/com/sparta/kanbanboard/domain/card/service/CardService.java
+++ src/main/java/com/sparta/kanbanboard/domain/card/service/CardService.java
@@ -17,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class CardService {
 
     private final CardAdapter cardAdapter;
+//    private final ColumnAdapter columnAdapter;
 
     // 컬럼이랑 연관관계 맺어서 수정해야할 것들 + user받아서 맨 밑에 시큐리티 로그인한 사용자랑 같은지 받는것만 고치면 됨(예외처리때문에)
     // 카드 전체 조회
@@ -31,9 +32,9 @@ public class CardService {
         return cardAdapter.findByUserId(userId);
     }
 
-    // 카드 상태별 조회
+    // 카드 상태별 조회 -> Column 데려와서 수정
    /* @Transactional
-    public List<Card> getCardsByStatus(CardStatusEnum status) {
+    public List<Card> getCardsByStatus(CardStatus status) {
         return cardAdapter.findByStatus(status);
     }
     */
@@ -46,7 +47,7 @@ public class CardService {
                 && card.getContents() == null /* && card.getStatus() == null*/) {
             throw new InvailCardDataException(ResponseExceptionEnum.INVALID_CARD_DATA);
         }
-        // Column validation (가정)
+        // 컬럼 ID를 통해 컬럼이 존재하는지 확인
         // Column column = columnRepository.findById(card.getColumnId())
         //     .orElseThrow(() -> new ColumnNotFoundException("Column not found"));
 
@@ -73,7 +74,7 @@ public class CardService {
 
         existingCard.setTitle(card.getTitle());
         existingCard.setContents(card.getContents());
-//        existingColumn.setStatus(columnStatusEnum.getStatus());
+//        **질문** 컬럼 상태 업데이트 필요시 추가하려는 로직인데 existingColumn.setStatus(columnStatus.getStatus()); 를 써야하는지 아니면  existingCard.setStatus(card.getStatus()); 를 써야할지 모르겠어요 Column에서 status enum 불러올겁니다..
 
         return cardAdapter.save(existingCard);
     }
@@ -94,6 +95,7 @@ public class CardService {
 
     // 로그인 사용자가 권한이 있는지 확인하는 메서드 ( 인증된 사용자 ID와 로그인사용자가 같아야 함 user랑 합치고 추가!)
     private boolean isLoggedInUserAuthorized(Long userId) {
+        // 여기에 실제 사용자 인증 로직 추가해야함
         return true;
     }
 
