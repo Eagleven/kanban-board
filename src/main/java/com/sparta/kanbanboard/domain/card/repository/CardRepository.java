6685f6a907b92bb58d43ee8ddcece3647e245c96
bbackbbacke <157469179+bbackbbacke@users.noreply.github.com>
2024-07-12 21:27:27 +0900

    fix : Column과 연관관계 맺고 findByColumn 추가

--- src/main/java/com/sparta/kanbanboard/domain/card/repository/CardRepository.java
+++ src/main/java/com/sparta/kanbanboard/domain/card/repository/CardRepository.java
@@ -2,6 +2,7 @@ package com.sparta.kanbanboard.domain.card.repository;
 
 import com.sparta.kanbanboard.common.CommonStatusEnum;
 import com.sparta.kanbanboard.domain.card.entity.Card;
+import com.sparta.kanbanboard.domain.column.entity.Column;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -14,5 +15,7 @@ public interface CardRepository extends JpaRepository<Card, Long> {
     List<Card> findByStatus(CommonStatusEnum status);
 
     List<Card> findByStatusAndUserId(CommonStatusEnum status, Long userId);
+
+    List<Card> findByColumn(Column column);
 
 }
