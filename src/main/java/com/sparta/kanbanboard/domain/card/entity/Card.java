3f002d02cec6acdb8bf8c2eafd18b983e739d4de
bbackbbacke <157469179+bbackbbacke@users.noreply.github.com>
2024-07-12 11:38:17 +0900

    fix : soft delete 위해 entity 수정(컬럼 추가, delete 메서드 추가)

--- src/main/java/com/sparta/kanbanboard/domain/card/entity/Card.java
+++ src/main/java/com/sparta/kanbanboard/domain/card/entity/Card.java
@@ -1,9 +1,12 @@
 package com.sparta.kanbanboard.domain.card.entity;
 
+import com.sparta.kanbanboard.common.CommonStatusEnum;
 import com.sparta.kanbanboard.common.TimeStampEntity;
 import com.sparta.kanbanboard.domain.user.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -33,11 +36,16 @@ public class Card extends TimeStampEntity {
     @Column(nullable = false)
     private String contents;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CommonStatusEnum status = CommonStatusEnum.ACTIVE;
+
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
 
     // Column : Card는 1:N 추가해야함
+    // @OneToMany
 
     @Builder
     public Card(String title, String contents, User user/*, Long columnId*/) {
@@ -50,5 +58,9 @@ public class Card extends TimeStampEntity {
     public void update(String title, String contents) {
         this.title = title;
         this.contents = contents;
+    }
+
+    public void delete() {
+        this.status = CommonStatusEnum.DELETED;
     }
 }
